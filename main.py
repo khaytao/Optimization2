ea7d602b76db31a4bd7ce0fdfd5616078c5b609d
Kfir Hayman <kfir.hayman@gmail.com>
2024-03-16 14:39:08 +0200

    added dy function

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import scipy
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+X1 = scipy.io.loadmat("X1.mat")
+Y = scipy.io.loadmat("Y.mat")
 
 
-# Press the green button in the gutter to run the script.
+def get_dx(m, n):
+    """
+    this function returns the dx matrix such that Dx x is the column stack representation of the spacial derivative in the x direction
+    :param m:
+    :param n:
+    :return:
+    """
+    pass
+
+
+def get_dy(m, n):
+    """
+    this function returns the dy matrix such that Dy x is the column stack representation of the spacial derivative in the y direction
+    :param m:
+    :param n:
+    :return:
+    """
+    dy = np.zeros([m * n, m * n])
+    for j in range(n):
+        for i in range(1, m): #iterate over the y direction
+            dy[j*m + i-1, j*m + i-1] = -1
+            dy[j*m +i-1, j*m + i] = 1
+    return dy
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dy = get_dy(3,3)
