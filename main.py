a38e6b9004fb0d6d39bef0699517cb86317e273e
Kfir Hayman <kfir.hayman@gmail.com>
2024-03-22 12:53:26 +0200

    q3 implementation

--- main.py
+++ main.py
@@ -5,14 +5,32 @@ X1 = scipy.io.loadmat("X1.mat")
 Y = scipy.io.loadmat("Y.mat")
 
 
+# def get_dx(m, n):
+#     """
+#     this function returns the dx matrix such that Dx x is the column stack representation of the spacial derivative in the x direction
+#     :param m:
+#     :param n:
+#     :return:
+#     """
+#     dx = np.zeros([m * n, m * n])
+#     for i in range(m):
+#         for j in range(n):
+#
+#             dx[i*n+i, j] = -1
+#             dx[j*(n-1), j + m] = 1
+#     return dx
+
 def get_dx(m, n):
-    """
-    this function returns the dx matrix such that Dx x is the column stack representation of the spacial derivative in the x direction
-    :param m:
-    :param n:
-    :return:
-    """
-    pass
+    dx = np.zeros([m * n, m * n])
+
+    j = 0
+
+    while j + m < m * n:
+        dx[j, j] = -1
+        dx[j, j + m] = 1
+        j+=1
+
+    return dx
 
 
 def get_dy(m, n):
@@ -22,12 +40,30 @@ def get_dy(m, n):
     :param n:
     :return:
     """
+    # dy = np.zeros([m * n, m * n])
+    # for j in range(n):
+    #     for i in range(1, m):  # iterate over the y direction
+    #         dy[j * m + i - 1, j * m + i - 1] = -1
+    #         dy[j * m + i - 1, j * m + i] = 1
+    # return dy
+
     dy = np.zeros([m * n, m * n])
-    for j in range(n):
-        for i in range(1, m): #iterate over the y direction
-            dy[j*m + i-1, j*m + i-1] = -1
-            dy[j*m +i-1, j*m + i] = 1
+
+    j = 0
+    while j //(m-1) + j < m*n:
+    #for j in range(m*n):
+        index = j //(m-1) + j
+        dy[index, index] = -1
+        dy[index, index + 1] = 1
+        j += 1
     return dy
 
 if __name__ == '__main__':
-    dy = get_dy(3,3)
+    m = 3
+    n = 2
+    Dy = get_dy(m, n)
+    Dx = get_dx(m, n)
+
+    x = np.arange(m * n)
+    dx = np.dot(Dx, x)
+    dy = np.dot(Dy, x)
